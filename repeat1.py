1305d40be1f45a6a82a3ca10ce930e0e1d6ff1d0
J. Antognini <antognini@astronomy.ohio-state.edu>
2015-11-05 18:30:56 -0500

    Continue rewrite of Set 1

--- repeat1.py
+++ repeat1.py
@@ -14,15 +14,17 @@ def hex2base64(a):
   data = binascii.unhexlify(a)
   return base64.b64encode(data)
 
-def fixedXOR(s1, s2):
-  '''Return the XOR of two hexadecimal strings, s1 and s2.'''
+def fixedXOR(b1, b2):
+  int1 = int(b1.encode('hex'), base=16)
+  int2 = int(b2.encode('hex'), base=16)
 
-  int1 = int(s1, base=16)
-  int2 = int(s2, base=16)
+  ret = binascii.unhexlify(hex(int1 ^ int2)[2:].rstrip('L'))
+  if len(ret) % 2 == 1:
+    ret = '0' + ret
 
-  return hex(int1 ^ int2)[2:].rstrip('L')
+  return ret
 
-def score_hex(s):
+def score_string(s):
   '''Return a score associated with how likely the string is given English
   character frequencies.'''
 
@@ -30,27 +32,30 @@ def score_hex(s):
   nonascii_score = -100
 
   score = 0
-  for idx in xrange(len(s) / 2):
-    b = s[2*idx:2*idx+2]
-    c = chr(int(b, base=16))
-    if c in en_log_freqs:
-      score += en_log_freqs[c]
-    elif 31 < ord(c) < 127:
+  for char in s:
+    if char in en_log_freqs:
+      score += en_log_freqs[char]
+    elif 31 < ord(char) < 127:
       score += rare_char_score
     else:
       score += nonascii_score
 
   return score
 
-def single_byte_xor(instring):
+def encrypt_single_key_xor(instring, key):
+  '''Encrypt a string against a repeating single character.'''
+
+  repeated_char = len(instring) * key
+  return fixedXOR(instring, repeated_char)
+
+def decrypt_single_key_xor(instring):
   '''Decode a string that has been encrypted against a single character.'''
 
   maxscore = None
   best_string = None
-  for i in xrange(128):
-    repeated_char = (len(instring)+1) / 2 * hex(i)[2:]
-    s = fixedXOR(instring, repeated_char)
-    score = score_hex(s)
+  for i in xrange(32, 128):
+    s = encrypt_single_key_xor(instring, chr(i))
+    score = score_string(s)
     if score > maxscore:
       maxscore = score
       best_string = s
@@ -66,12 +71,53 @@ def detect_single_char_xor(filename):
   with open(filename) as infile:
     lines = infile.readlines()
 
-  for line in lines:
-    decoded_line = single_key_xor(line)
-    if score_hex(decoded_line) > maxscore:
+  for raw_line in lines:
+    line = raw_line.strip().decode('hex')
+    decoded_line = decrypt_single_key_xor(line)
+    score = score_string(decoded_line) 
+    if score > maxscore:
       best_line = decoded_line
+      maxscore = score
 
   return best_line
+
+def repeating_key_xor(s, key):
+  '''Encrypt a string s with the repeating key.'''
+
+  key_len = len(key)
+  enc_blocks = []
+  for i in range(key_len):
+    decrypt_block = encrypt_single_key_xor(s[i::key_len], key[i])
+    enc_blocks.append(decrypt_block)
+
+  enc_block = [elem for sublist in zip(*enc_blocks) for elem in sublist]
+  enc_block = ''
+  for i in xrange(len(enc_blocks[0])):
+    for block in enc_blocks:
+      try:
+        enc_block += block[i]
+      except IndexError:
+        pass
+  return enc_block.encode('hex')
+
+def hamming_distance(s1, s2):
+  '''Compute the Hamming distance between s1 and s2.'''
+
+  int1 = int(s1, base=16)
+  int2 = int(s2, base=16)
+  xored = int1 ^ int2
+  hamming_distance = 0
+  while xored:
+    if xored % 2 == 1:
+      hamming_distance += 1
+    xored >>= 1
+
+  return hamming_distance
+
+def find_keysize(data):
+  '''Find the keysize used to encrypt the data.'''
+
+
 
 if __name__ == '__main__':
   # Challenge 1
@@ -85,18 +131,30 @@ if __name__ == '__main__':
   STRING1_2A = '1c0111001f010100061a024b53535009181c'
   STRING1_2B = '686974207468652062756c6c277320657965'
   RESULT1_2  = '746865206b696420646f6e277420706c6179'
-  assert fixedXOR(STRING1_2A, STRING1_2B) == RESULT1_2
+  assert fixedXOR(STRING1_2A.decode('hex'), 
+    STRING1_2B.decode('hex')).encode('hex') == RESULT1_2
   print "Challenge 2 test passed"
   print
 
   # Challenge 3
   STRING1_3 = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
   print "Challenge 3 solution:"
-  print single_byte_xor(STRING1_3).decode('hex')
+  print decrypt_single_key_xor(STRING1_3.decode('hex'))
   print
 
   # Challenge 4
   print "Challenge 4 solution:"
-  print detect_singchar_xor('set1-4.txt'),
+  print detect_single_char_xor('set1-4.txt')
+
+  # Challenge 5
+  STRING1_5A = 'Burning \'em, if you ain\'t quick and nimble\n'
+  STRING1_5B = 'I go crazy when I hear a cymbal'
+  STRING1_5 = STRING1_5A + STRING1_5B
+  STRING1_5KEY = 'ICE'
+  RESULT1_5A = '0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272'
+  RESULT1_5B = 'a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f'
+  RESULT1_5 = RESULT1_5A + RESULT1_5B
+  assert repeating_key_xor(STRING1_5, STRING1_5KEY) == RESULT1_5
+  print "Challenge 5 test passed"
   print
 
